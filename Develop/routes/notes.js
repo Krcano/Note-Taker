5423c753c6ddbbfaa9280fbd09a032c304797589
Kelly R Cano <krcano2001@gmail.com>
2021-10-27 13:06:31 -0800

    added license installation and usage instructions to readme

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,39 +1,65 @@
 const router = require("express").Router();
+const { error } = require("console");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
 // Post routes
 router.post("/notes", async (req, res) => {
   console.info(`${req.method} request received to add notes`);
-//await is making sure the db.json file is read before it assigns it to the variable and executes the rest of the code
+  //await is making sure the db.json file is read before it assigns it to the variable and executes the rest of the code
   const notes = await fs.promises.readFile("./db/db.json", "utf-8");
   const notesArray = JSON.parse(notes);
-//   assigns an unique id and calls the process newID
+  // assigns an unique id and calls the process newID
   const newId = uuidv4();
-//Names the req.body newNote
+  //Names the req.body newNote
   const newNote = req.body;
-//makes the newId(uuid) the newNote(which is req.body).id
+  //makes the newId(uuid) the newNote(which is req.body).id
   newNote.id = newId;
   console.log(notes);
   console.log(newNote);
   notesArray.push(newNote);
-//await is making sure the db.json file is written before it refreshes the page
+  //await is making sure the db.json file is written before it refreshes the page
   await fs.promises.writeFile("./db/db.json", JSON.stringify(notesArray));
   res.redirect("back");
 });
 
-router.get('/notes', async (req, res) => {
-    // makes sure the db.json file is read beofre it sends back the parsed notes to /notes
-    const notes = await fs.promises.readFile("./db/db.json", "utf-8");
-    res.send(JSON.parse(notes))
-})
-router.delete('/notes/:id', (req, res)=>{
-    const id = req.params.id;
-    // 1 read file, parse into json, create an array from parsed json,
-    // find the index of the object that has the id, splice index out of the area, use index to delete that object form the array using splice method
-    // write file again, redirect 'back'
+router.get("/notes", async (req, res) => {
+  // makes sure the db.json file is read beofre it sends back the parsed notes to '/notes'
+  const notes = await fs.promises.readFile("./db/db.json", "utf-8");
+  res.send(JSON.parse(notes));
+});
 
+router.delete("/notes/:id", async(req, res, next) => {
+  // const id = req.params.id;
+  // 1 read file, parse into json,
+  // 2 create an array from parsed json,
+  // 3 find the index of the object that has the id using array method: findIndex() and splice index out of the array,
+  // use index to delete that object form the array using splice method
+  // 4 write file again, redirect 'back'
 
-    // array method: findIndex()
-})
+  // let parsedNotesArray = [];
+  // const parsedNotesArray = await fs.promises.readFile("./db/db.json", "utf-8");
+  
+  // const indexOfdeleted =  parsedNotesArray.indexOf((deletedNote) => deletedNote.id === req.params.id )
+  // if (indexOfdeleted === req.params.id) {
+  //   parsedNotesArray.splice(indexOfdeleted, 1);
+  //   return  console.log(indexOfdeleted)
+  // }
+  // }else{console.log(error)}
+
+  // 1st attempt not working
+  // const notes = JSON.parse(fs.readFile("./db/db.json"));
+  // const deleted = notes.filter((deletedNote)=> deletedNote.id !== req.params.id)
+  // fs.writeFile("./db/db.json", JSON.stringify(deleted));
+  // res.json(deleted)
+
+  // 2nd attempt not working
+  //   const currentIndex = notesArray.filter(idIndex);
+  //   if(idIndex !== currentIndex ){
+  //     notesArray.splice(idIndex, 1);
+  //     res.status(204).send
+  //   }else{
+  //     res.status(404).send()
+  //   }
+});
 module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Note-Taker
+
+![License: MIT](https://img.shields.io/badge/License-MIT-success.svg)
+
+## Description
+
+
+## Installation
+
+To install the project the user must have Node.js and the Develop folder with this code base downloaded on their computer and then go to the correct file where the project is located. After you will start your server and go to the deployed page on your browser.
+
+## Usage
+
+To get started press the 'Get Started' button that will take you to the notes page. then press the the '+' button on the top right corner of your screen to make a new note and and type away. After you're done writing your note a save button will pop up and you will have the option to save your note. Once you click save you you will see your new note pop up on the left side of your screen.
+
+## License
+
+MIT License
+
+Copyright (c) [2021] [Kelly R. Cano]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
